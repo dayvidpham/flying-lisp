15d29e2bff867178f15661fc3e772ff2af4199c7
dhpham <dayvidpham@gmail.com>
2020-11-07 19:27:49 -0800

    Adds rendering of fly and asteroid

--- lib/asteroid.dart
+++ lib/asteroid.dart
@@ -1,22 +1,24 @@
 import 'dart:ui';
-import 'package:flame/box2d/box2d_component.dart';
 
+import 'package:flame/components/component.dart';
+import 'package:flame/components/composed_component.dart';
+import 'package:flame/flame.dart';
 import 'package:box2d_flame/box2d.dart' as Box2D;
 import 'package:flame/box2d/box2d_component.dart';
 import 'package:flame/sprite.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
 
-class FlyComponent extends BodyComponent {
+class AsteroidComponent extends BodyComponent {
   Sprite defaultSprite;
 
-  FlyComponent(Box2DComponent box) : super(box) {
+  AsteroidComponent(Box2DComponent box) : super(box) {
     _loadImage();
     _createBody();
   }
 
   void _loadImage() {
-    this.defaultSprite = new Sprite('fly.png');
+    this.defaultSprite = new Sprite('asteroid1.png');
   }
 
   void _createBody() {
@@ -25,6 +27,7 @@ class FlyComponent extends BodyComponent {
     bodyDef.position =
         Box2D.Vector2(box.dimensions.width / 2, box.dimensions.height / 2);
     bodyDef.angle = 0;
+    bodyDef.linearVelocity = new Box2D.Vector2(-5.0, 0.0);
 
     this.body = box.world.createBody(bodyDef);
   }
--- lib/lisp-game.dart
+++ lib/lisp-game.dart
@@ -2,20 +2,24 @@ import 'dart:ui';
 import 'package:flame/box2d/box2d_component.dart';
 import 'package:flame/box2d/box2d_game.dart';
 import 'package:flame/flame.dart';
+import 'package:lisp/asteroid.dart';
+import 'package:lisp/fly.dart';
 import 'background.dart';
 
 class LispGame extends Box2DGame {
   Size screenSize;
   Background background;
-  Box2DComponent box;
+  final Box2DComponent box;
 
-  LispGame(Box2DComponent box) : super(box) {
+  LispGame(this.box) : super(box) {
     initialize();
   }
 
   void initialize() async {
     this.resize(await Flame.util.initialDimensions());
     this.background = new Background(this);
+    this.add(new AsteroidComponent(box));
+    this.add(new FlyComponent(box));
   }
 
   void render(Canvas canvas) {
