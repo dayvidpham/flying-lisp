b91828fad2a16078bc04a1eab96bd5f9d398258e
lucyxhz <lucy_zhong@sfu.ca>
2020-11-07 16:31:38 -0800

    wip on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flame/util.dart';
-
-import 'lisp-game.dart';
+import 'package:LISP/lisp-game.dart';
+import 'dart:async';
 
 void main() async {
+  await setupFlame();
   var game = new LispGame();
   runApp(game.widget);
-
-  setupFlame();
 }
 
-void setupFlame() {
+void setupFlame() async {
   Util flameUtil = Util();
-  flameUtil.fullScreen();
-  flameUtil.setLandscape();
+  await flameUtil.fullScreen();
+  await flameUtil.setLandscape();
 }
