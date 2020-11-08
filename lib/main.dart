50a2c19c0157d1ef87f45b0d0f21387489e9b29f
dhpham <dayvidpham@gmail.com>
2020-11-07 16:30:02 -0800

    Revert "minor changes to main.dart"
    This reverts commit c4503eacc28cb357215c8b16c71d69ad3f85ad67.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flame/util.dart';
-import 'dart:async';
+
 import 'homepage.dart';
 
 void main() {
-  // await setupFlame();
   runApp(MyApp());
 }
 
